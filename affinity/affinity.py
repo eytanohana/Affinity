e40ad79639d744ab6e57a265a50c45b5e6a7e764
eytanohana <eohana98@gmail.com>
2022-10-26 01:11:00 +0300

    rename method

--- affinity/affinity.py
+++ affinity/affinity.py
@@ -10,7 +10,7 @@ class Affinity:
         self.session = Session()
         self.session.auth = HTTPBasicAuth('', api_key)
 
-    def get_all_lists(self) -> list[models.List]:
+    def get_lists(self) -> list[models.List]:
         response = self.session.get(urls.LISTS)
         if response.ok:
             return [models.List(**ls) for ls in response.json()]
@@ -18,7 +18,7 @@ class Affinity:
             response.raise_for_status()
 
     def get_list_by_name(self, name: str) -> models.List | None:
-        all_lists = self.get_all_lists()
+        all_lists = self.get_lists()
         for ls in all_lists:
             if ls.name == name:
                 return ls
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     load_dotenv(Path(__file__).parent / '.env')
     affinity_key = os.environ.get('AFFINITY_KEY')
     af = Affinity(api_key=affinity_key)
-    all_lists = af.get_all_lists()
+    all_lists = af.get_lists()
     specific_list = af.get_list_by_name('Davids Affinity Integrations Testing')
     specific_list_by_id = af.get_list_by_id(specific_list.id)
     print()
